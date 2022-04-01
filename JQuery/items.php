78a4a22ac8d58e4d0ad13c41111377cb2f123125
ayahassan <eng.ayaa2050@gmail.com>
2022-04-01 21:52:26 +0200

    try-jquery

---
+++ JQuery/items.php
@@ -0,0 +1,48 @@
+<?php
+	$items = [
+		[
+			'name' => ' Double Whopper ',
+			'price' => 10,
+			'image' => 'images/1.jpg',
+            'description' => ' Bun 5 inch, 2 pcs whopper patties, tomatoes, lettuce, onion, pickles, ketchup and mayonnaise '
+		],
+		[
+			'name' => ' Whopper ',
+			'price' => 70,
+			'image' => 'images/2.jpg',
+            'description' => ' Bun 4.5 inch, whopper patty, American cheese, beef bacon, mayonnaise, lettuce, tomato, fried onion '
+
+		],
+		[
+			'name' => ' Chicken Royal ',
+			'price' => 30,
+			'image' => 'images/3.jpg',
+            'description' => ' Bun 7 inch, chicken royal patty, lettuce and mayonnaise '
+		],
+        [
+			'name' => '   Swiss Mushroom Single   ',
+			'price' => 30,
+			'image' => 'images/4.jpg',
+            'description' => ' Bun 5 inch, whopper patty, 2 Swiss cheese, mushroom sauce '
+		],
+        [
+			'name' => '  Chicken Burger  ',
+			'price' => 30,
+			'image' => 'images/5.jpg',
+            'description' => ' Bun 4 inch, chicken burger patty, lettuce and mayonnaise '
+		],
+        [
+			'name' => ' Steakhouse Beef ',
+			'price' => 30,
+			'image' => 'images/6.jpg',
+            'description' => ' Bun 4.5 inch, whopper patty, American cheese, beef bacon, mayonnaise, lettuce, tomato, fried onion '
+		]
+	];
+
+
+	header('Content-type: application/json');
+
+   // echo count($items);
+   echo json_encode($items);
+	die();
+	//header('HTTP/1.0 403 Forbidden');
