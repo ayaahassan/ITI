2e67b3d9b0fa71086587a5bb58a73bfb51043379
ayahassan <eng.ayaa2050@gmail.com>
2022-06-04 02:58:27 +0200

    django-lab3

--- django/lab1/djangoProject/todo/urls.py
+++ django/lab1/djangoProject/todo/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import todo_home, todo_json, todo_list,todo_detail,todo_done,todo_delete
+from .views import todo_home, todo_json, todo_list,todo_detail,todo_done,todo_delete,todo_update
 app_name = 'todo'
 
 urlpatterns = [
@@ -7,6 +7,7 @@ urlpatterns = [
     path('detail/<str:todo_name>', todo_detail,name='detail'),
     path('done/<str:todo_name>', todo_done, name='done'),
     path('delete/<str:todo_name>', todo_delete, name='delete'),
+    path('update/<str:todo_name>', todo_update, name='update'),
     path('home/json', todo_json, name='home_json'),
     path('home/<str:todo_name>', todo_home, name='home')
 ]
